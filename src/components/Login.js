e2ca74793eec284fb3e168dc3b41930f707b004c
Joseph Gordy <jgordy2424@gmail.com>
2017-12-06 09:53:57 -0500

    added some text to the login and app components. Testing git tools on windows as well

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
   render() {
     return(
       <div className='Login'>
-        <h3>Login</h3>
+        <h1 style={{textAlign: "center"}}>Login Component</h1>
       </div>
     )
   }
