e1d99db30afaad0cc025dc9a76445fca9a918296
Joseph Gordy <jgordy2424@gmail.com>
2017-12-06 12:04:28 -0500

    added a form to the login component. Non functioning for now.

--- src/components/CauseList.js
+++ src/components/CauseList.js
@@ -4,25 +4,40 @@ import { Link } from 'react-router-dom';
 export default class CauseList extends Component {
 
   render() {
+
+    let causeStyle = {
+      // border: '1px solid rgba(100,100,100,0.6)',
+      // borderRadius: '3px',
+      height: '3rem',
+      margin: '3rem'
+    }
+
+    let linkStyle = {
+      border: '3px solid rgba(50,50,50,0.2)',
+      borderRadius: '3px',
+      padding: '1rem 1.5rem',
+      textDecoration: 'none'
+    }
+
     return (
       <div className="CauseList">
 
         <h1 style={{textAlign: 'center'}}>Cause List Component</h1>
 
-        <div>
-          <Link to='/causes/1'>Cause 1</Link>
+        <div className="causes" style={causeStyle}>
+          <Link to='/causes/1' style={linkStyle}>Cause 1</Link>
         </div>
-        
-        <div>
-          <Link to='/causes/2'>Cause 2</Link>
+
+        <div className="causes" style={causeStyle}>
+          <Link to='/causes/2' style={linkStyle}>Cause 2</Link>
         </div>
-        
-        <div>
-          <Link to='/causes/3'>Cause 3</Link>
+
+        <div className="causes" style={causeStyle}>
+          <Link to='/causes/3' style={linkStyle}>Cause 3</Link>
         </div>
-        
-        <div>
-          <Link to='/causes/4'>Cause 4</Link>
+
+        <div className="causes" style={causeStyle}>
+          <Link to='/causes/4' style={linkStyle}>Cause 4</Link>
         </div>
 
       </div>
