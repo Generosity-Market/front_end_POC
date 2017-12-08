c5fc587d05a6d424f7877f81794ad1db81f27c4a
Joseph Gordy <jgordy2424@gmail.com>
2017-12-08 08:42:34 -0500

    added sample data to work on the front end until api is up and running

--- src/components/Campaign/Campaign.js
+++ src/components/Campaign/Campaign.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 export default class Campaign extends Component {
 
   render() {
+    console.log(this.props.match.params.id);
+    let id = this.props.match.params.id;
     return (
       <div className="Campaign">
         <h1 style={{textAlign: 'center'}}>Campaign Component</h1>
-        <p>Campaign details</p>
+        <p>Campaign <span style={{color: 'red'}}>{id}</span> details</p>
       </div>
     )
   }
--- src/components/CauseList/CauseList.js
+++ src/components/CauseList/CauseList.js
@@ -1,42 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import causeNames from '../../data.js';
+
 export default class CauseList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      causes: causeNames,
+      message: "there are no causes"
+    }
+  }
 
   render() {
 
     let causeStyle = {
+      // backgroundColor: 'lightgray',
       height: '3rem',
-      margin: '3rem'
+      margin: '3rem',
+      display: 'flex'
     };
 
     let linkStyle = {
       border: '3px solid rgba(50,50,50,0.2)',
       borderRadius: '3px',
-      padding: '1rem 1.5rem',
-      textDecoration: 'none'
+      color: 'darkslategray',
+      padding: '0.75rem 0rem',
+      textDecoration: 'none',
+      flexBasis: '30%',
+      textAlign: 'center',
+      verticalAlign: 'center'
     };
+
+    let displayedStuff;
+    if (this.state.causes) {
+      displayedStuff = this.state.causes.map((cause) => {
+        return <div key={cause.id} className="causes" style={causeStyle}>
+                <Link to={`/causes/${cause.id}`} style={linkStyle}>{cause.name}</Link>
+              </div>;
+      })
+    } else {
+      displayedStuff = this.state.message;
+    }
+
 
     return (
       <div className="CauseList">
 
         <h1 style={{textAlign: 'center'}}>Cause List Component</h1>
 
-        <div className="causes" style={causeStyle}>
-          <Link to='/causes/1' style={linkStyle}>Cause 1</Link>
-        </div>
-
-        <div className="causes" style={causeStyle}>
-          <Link to='/causes/2' style={linkStyle}>Cause 2</Link>
-        </div>
-
-        <div className="causes" style={causeStyle}>
-          <Link to='/causes/3' style={linkStyle}>Cause 3</Link>
-        </div>
-
-        <div className="causes" style={causeStyle}>
-          <Link to='/causes/4' style={linkStyle}>Cause 4</Link>
-        </div>
+        {displayedStuff}
 
       </div>
     )
