eea99c8e42d7ea02c4cf22ff77241d0e7553095e
Joseph Gordy <jgordy2424@gmail.com>
2017-12-08 10:43:11 -0500

    added the amount to raise and the number of envelopes needed to raise that amount to a banner at the top of the screen

--- src/components/Campaign/Campaign.js
+++ src/components/Campaign/Campaign.js
@@ -33,7 +33,6 @@ export default class Campaign extends Component {
         envelopeNumber = 1,
         amount = this.state.amount;
 
-
     while (amount > 0) {
 
       envelopeContainer.push({amount: amount, envelopeNumber: envelopeNumber})
@@ -42,7 +41,7 @@ export default class Campaign extends Component {
     }
 
     let envelopeDisplay = envelopeContainer.map((cause, index) => {
-      return <div key={index} className="envelope" style={{flexBasis: '20%'}}>
+      return <div key={index} className="envelope" style={{flexBasis: '12%', margin: '0.25rem', backgroundColor: '#efefff'}}>
               <h3>{cause.envelopeNumber}</h3>
              </div>;
     })
@@ -50,12 +49,14 @@ export default class Campaign extends Component {
     return (
       <div className="Campaign">
         <h1 style={{textAlign: 'center'}}>Campaign Component</h1>
-        <p>{this.state.name} <span style={{color: 'red'}}>{this.state.id}</span> details</p>
+        <p>{this.state.name} <span style={{color: 'red'}}>id: {this.state.id}</span> details</p>
 
-        <p>Amount ro raise: ${this.state.amount}</p>
-        <p>Envelope Amount: {envelopeNumber - 1}</p>
+        <div className="campaignBanner" style={{display: 'flex', justifyContent: 'space-around', backgroundColor: 'gray', color: 'white', position: 'sticky', top: '0'}}>
+          <p>Amount ro raise: ${this.state.amount}</p>
+          <p>Envelope Amount: {envelopeNumber - 1}</p>
+        </div>
 
-        <div className="envelopeDisplay" style={{display: 'flex', flexFlow: 'row wrap'}}>
+        <div className="envelopeDisplay" style={{display: 'flex', flexFlow: 'row wrap', width: '90%', margin: '4rem auto', justifyContent: 'space-around', textAlign: 'center'}}>
           {envelopeDisplay}
         </div>
 
