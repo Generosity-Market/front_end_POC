0d485021b0b0d9d2ed0fd1696321925e73141b18
Joseph Gordy <jgordy2424@gmail.com>
2017-12-09 12:22:31 -0500

    added line breaks to create sections in the envelope display area. Added small amount of layout styling

--- src/components/Campaign/Campaign.js
+++ src/components/Campaign/Campaign.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import causeNames from '../../data.js';
+import './Campaign.css';
 
 export default class Campaign extends Component {
   constructor(props) {
@@ -9,14 +10,13 @@ export default class Campaign extends Component {
       id: '',
       name: '',
       amount: 0
-    }
-  }
+    };
+  };
 
   componentDidMount() {
     let id = this.props.match.params.id;
 
     for (var i = 0; i < causeNames.length; i++) {
-
       if (causeNames[i].id === Number(id)) {
         this.setState({
           id: causeNames[i].id,
@@ -25,7 +25,7 @@ export default class Campaign extends Component {
         })
       }
     }
-  }
+  };
 
   render() {
 
@@ -35,23 +35,36 @@ export default class Campaign extends Component {
 
     while (amount > 0) {
 
-      envelopeContainer.push({amount: amount, envelopeNumber: envelopeNumber})
-      amount = amount - envelopeNumber;
-    envelopeNumber++;
-    }
+      if (envelopeNumber % 21 === 0) {
+        envelopeContainer.push({amount: amount, envelopeNumber: envelopeNumber})
+        envelopeContainer.push({name: "lineBreak"});
+        amount = amount - envelopeNumber;
+        envelopeNumber++;
+      } else {
+        envelopeContainer.push({amount: amount, envelopeNumber: envelopeNumber})
+        amount = amount - envelopeNumber;
+        envelopeNumber++;
+      };
+    };
 
     let envelopeDisplay = envelopeContainer.map((cause, index) => {
-      return <div key={index} className="envelope" style={{flexBasis: '12%', margin: '0.25rem', backgroundColor: '#efefff'}}>
-              <h3>{cause.envelopeNumber}</h3>
-             </div>;
-    })
+
+      if (cause.name === "lineBreak") {
+        return <div key={index} className="lineBreak" style={{flexBasis: '100%'}}><hr/><br/></div>
+      } else {
+        return <div key={index} className="envelope" style={{ margin: '0.25rem'}}>
+                <h3 style={{margin: '0.6rem 0rem'}}>${cause.envelopeNumber}</h3>
+               </div>;
+      };
+    });
 
     return (
       <div className="Campaign">
+
         <h1 style={{textAlign: 'center'}}>Campaign Component</h1>
         <p>{this.state.name} <span style={{color: 'red'}}>id: {this.state.id}</span> details</p>
 
-        <div className="campaignBanner" style={{display: 'flex', justifyContent: 'space-around', backgroundColor: 'gray', color: 'white', position: 'sticky', top: '0'}}>
+        <div className="campaignBanner" style={{display: 'flex', justifyContent: 'space-around', backgroundColor: '#3b653d', color: 'white', position: 'sticky', top: '0', zIndex: '1'}}>
           <p>Amount ro raise: ${this.state.amount}</p>
           <p>Envelope Amount: {envelopeNumber - 1}</p>
         </div>
--- src/components/CauseList/CauseList.js
+++ src/components/CauseList/CauseList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import causeNames from '../../data.js';
+import './CauseList.css';
 
 export default class CauseList extends Component {
   constructor(props) {
@@ -21,21 +22,11 @@ export default class CauseList extends Component {
       display: 'flex'
     };
 
-    let linkStyle = {
-      border: '3px solid rgba(50,50,50,0.2)',
-      borderRadius: '3px',
-      color: 'darkslategray',
-      padding: '0.75rem 0rem',
-      textDecoration: 'none',
-      flexBasis: '30%',
-      textAlign: 'center'
-    };
-
     let displayedStuff;
     if (this.state.causes) {
       displayedStuff = this.state.causes.map((cause) => {
         return <div key={cause.id} className="causes" style={causeStyle}>
-                <Link to={`/causes/${cause.id}`} style={linkStyle}>{cause.name}</Link>
+                <Link to={`/causes/${cause.id}`}>{cause.name}</Link>
               </div>;
       })
     } else {
