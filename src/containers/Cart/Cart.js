1b88de6a8d6c55a11b9164dc8c6aacefd6077215
Joseph Gordy <jgordy2424@gmail.com>
2017-12-15 15:34:36 -0500

    Completed the clear cart button functionality, including killing the bugs that it introduced.

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -3,8 +3,17 @@ import './Cart.css';
 
 export default class Cart extends Component {
 
+
+  clearCart = () => {
+    let cart = this.props.cart.length;
+    // reverse for loop bc we are removing items from the array
+    // normal for loop skips even numbered indexes...so we skipped it
+    for (let i = cart - 1; i >= 0; i -= 1) {
+      document.querySelector(`.amount${this.props.cart[i]}`).click();
+    }
+  };
+
   render() {
-    // console.log(this.props.removeFromCart);
     let cartTotal;
 
     if (this.props.cart.length === 0) {
@@ -22,11 +31,19 @@ export default class Cart extends Component {
 
 
     return <div className="Cart">
-            <div className="item-container">
-              <h4>Cart</h4>
-              {cartItems}
-            </div>
-            <h3>Total: ${cartTotal}</h3>
+
+             <div className="item-container">
+               <h3>Cart</h3>
+               {cartItems}
+             </div>
+
+             <div className="cart-info">
+               <h3>Total: ${cartTotal}</h3>
+               <h4 className="clear-cart" onClick={() => {
+                 this.clearCart();
+               }}>Clear Cart</h4>
+             </div>
+
            </div>
   }
 };
